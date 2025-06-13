7e617ade22660928fdad169ad806d95a65257774
Matei Ionescu <matei.ionescu7@gmail.com>
2025-06-13 17:57:28 +0300

    use eeprom update instead of write

--- Software/RootKB-Left-Software/src/data_manager.cpp
+++ Software/RootKB-Left-Software/src/data_manager.cpp
@@ -10,26 +10,26 @@ namespace data_manager {
     }
 
     void save_rgb(uint8_t mode, uint8_t hue, uint8_t saturation, uint8_t brightness) {
-        EEPROM.write(0, mode);
-        EEPROM.write(1, hue);
-        EEPROM.write(2, saturation);
-        EEPROM.write(3, brightness);
+        EEPROM.update(0, mode);
+        EEPROM.update(1, hue);
+        EEPROM.update(2, saturation);
+        EEPROM.update(3, brightness);
     }
 
     void save_rgb_mode(const uint8_t mode) {
-        EEPROM.write(0, mode);
+        EEPROM.update(0, mode);
     }
 
     void save_rgb_hue(const uint8_t hue) {
-        EEPROM.write(1, hue);
+        EEPROM.update(1, hue);
     }
 
     void save_rgb_saturation(const uint8_t saturation) {
-        EEPROM.write(2, saturation);
+        EEPROM.update(2, saturation);
     }
 
     void save_rgb_brightness(const uint8_t brightness) {
-        EEPROM.write(3, brightness);
+        EEPROM.update(3, brightness);
     }
 
     uint8_t get_rgb_mode() {
@@ -53,7 +53,7 @@ namespace data_manager {
         for (uint8_t layer = 0; layer < keys::LAYERS_NUM; ++layer) {
             for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
                 for (uint8_t col = 0; col < MATRIX_COLS_BOTH; ++col) {
-                    EEPROM.write(pos++, layout[layer][row][col]);
+                    EEPROM.update(pos++, layout[layer][row][col]);
                 }
             }
         }
--- Software/RootKB-Right-Software/src/data_manager.cpp
+++ Software/RootKB-Right-Software/src/data_manager.cpp
@@ -9,26 +9,26 @@ namespace data_manager {
     }
 
     void save_rgb(uint8_t mode, uint8_t hue, uint8_t saturation, uint8_t brightness) {
-        EEPROM.write(0, mode);
-        EEPROM.write(1, hue);
-        EEPROM.write(2, saturation);
-        EEPROM.write(3, brightness);
+        EEPROM.update(0, mode);
+        EEPROM.update(1, hue);
+        EEPROM.update(2, saturation);
+        EEPROM.update(3, brightness);
     }
 
     void save_rgb_mode(uint8_t mode) {
-        EEPROM.write(0, mode);
+        EEPROM.update(0, mode);
     }
 
     void save_rgb_hue(uint8_t hue) {
-        EEPROM.write(1, hue);
+        EEPROM.update(1, hue);
     }
 
     void save_rgb_saturation(uint8_t saturation) {
-        EEPROM.write(2, saturation);
+        EEPROM.update(2, saturation);
     }
 
     void save_rgb_brightness(uint8_t brightness) {
-        EEPROM.write(3, brightness);
+        EEPROM.update(3, brightness);
     }
 
     uint8_t get_rgb_mode() {
